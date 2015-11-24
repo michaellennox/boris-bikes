42c8f77baaea8f74084e6369748281f647e42f91
michaellennox <michaelctlennox@gmail.com>
2015-11-24 17:03:24 +0000

    Added test to check error is raised if docking station is full when trying to dock another bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
   describe '#release_bike' do
     it { is_expected.to respond_to(:release_bike) }
     it 'releases a working bike if a bike is present' do
@@ -13,11 +14,22 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error('No bike available!')
     end
   end
-  it { is_expected.to respond_to(:dock).with(1).argument}
-  it { is_expected.to respond_to(:bikes) }
-  it 'accepts a bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq(bike)
+
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument}
+    it 'accepts a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to eq(bike)
+    end
+    it 'does not accept a bike if it is full' do
+      bike1 = Bike.new
+      subject.dock(bike1)
+      bike2 = Bike.new
+      expect { subject.dock(bike2) }.to raise_error('Docking station is full!')
+    end
   end
+
+  it { is_expected.to respond_to(:bikes) }
+
 end
