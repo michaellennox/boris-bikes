922e0b68264ba471f3978c7fd045aec0dcf7051a
michaellennox <michaelctlennox@gmail.com>
2015-11-24 15:20:50 +0000

    Wrote test to check that DockingStation responds to dock method with an argument

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,10 @@
 require 'docking_station'
+
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-  it 'releases a working bike' do 
+  it 'releases a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it { is_expected.to respond_to(:dock).with(1).argument}
 end
