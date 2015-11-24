8c646dba5804d62e7e335ab31a04eeb0b7c5d898
giamir <giamir.buoncristiani@gmail.com>
2015-11-24 19:20:12 +0000

    challenge 14 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,19 @@ class DockingStation
     @bikes = []
   end
 
+  def full?
+    @bikes.size >= 20 ? true : false
+  end
+
   def release_bike
     fail 'No bikes available' if @bikes.empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.size >= 20
+    fail 'Docking station is full' if full?
     @bikes << bike
   end
+
+  private :full?
 end
