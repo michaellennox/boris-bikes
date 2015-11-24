eece00ca584955b0869bfde82b841b5984a029bf
emmabeynon <emma.beynon@gmail.com>
2015-11-24 12:58:34 +0000

    Fixed require command and refactored spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it 'releases a working bike' do 
-    bike = DockingStation.new.release_bike
+    bike = subject.release_bike
     expect(bike.working?).to be_truthy
   end
 end
