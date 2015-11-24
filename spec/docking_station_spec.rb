4ce044e4aca39dc5a57ae3855619e28704e34bba
Jelgar <jonathanelgardiner@gmail.com>
2015-11-24 17:42:19 +0000

    First 10 steps

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+  it 'to get bike out and working' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+end
