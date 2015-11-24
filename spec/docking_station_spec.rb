e3ed098848e23dfc25e292b70e0e6740ebe6eb32
emmabeynon <emma.beynon@gmail.com>
2015-11-24 12:37:32 +0000

    Created test to get bike and expect bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  it 'releases a working bike' do 
+    bike = DockingStation.new.release_bike
+    expect(bike.working?).to be_truthy
+  end
 end
