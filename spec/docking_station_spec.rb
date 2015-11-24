a42c8ea8237d7ae7feba2811c8008c73bf2ae477
emmabeynon <emma.beynon@gmail.com>
2015-11-24 17:36:22 +0000

    Edited test to account for 20 bikes capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,7 @@ describe DockingStation do
       expect(DockingStation.new.bikes).to eq([])
     end
   end
-  
+
   describe '#release_bike' do
     it { is_expected.to respond_to(:release_bike) }
     it 'releases a working bike if a bike is present' do
@@ -24,10 +24,10 @@ describe DockingStation do
     it 'accepts a bike' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bikes).to eq(bike)
+      expect(subject.bikes).to eq([bike])
     end
     it 'does not accept a bike if it is full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error('Docking station is full!')
     end
   end
