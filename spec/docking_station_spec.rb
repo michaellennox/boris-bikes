44d0cacc0fd63bd33aad5334eb3e5491b1a7e859
emmabeynon <emma.beynon@gmail.com>
2015-11-24 18:07:05 +0000

    Changed 20 to DEFAULT_CAPACITY in tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -27,7 +27,7 @@ describe DockingStation do
       expect(subject.bikes).to eq([bike])
     end
     it 'does not accept a bike if it is full' do
-      20.times { subject.dock(Bike.new) }
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error('Docking station is full!')
     end
   end
