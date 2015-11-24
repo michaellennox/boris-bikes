e9755afe91fe4be64b244ec0fd7867b6b5cac4e9
michaellennox <michaelctlennox@gmail.com>
2015-11-24 15:42:23 +0000

    Refactored docking_station tests slightly

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
@@ -9,8 +10,7 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument}
   it { is_expected.to respond_to(:bikes) }
   it 'accepts a bike' do
-    bike = subject.release_bike
-    subject.dock(bike)
-    expect(subject.bikes).to eq(bike)
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
   end
 end
