d9c0c26a4062e5485387a70ef738fa43f483c707
emmabeynon <emma.beynon@gmail.com>
2015-11-24 16:52:35 +0000

    Refactored #release_bike tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,16 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-  it 'releases a working bike if a bike is present' do
-    bike1 = Bike.new
-    subject.dock(bike1)
-    expect(subject.release_bike).to be_working
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike) }
+    it 'releases a working bike if a bike is present' do
+      bike1 = Bike.new
+      subject.dock(bike1)
+      expect(subject.release_bike).to be_working
+    end
+    it 'raises an error if no bike available' do
+      expect { subject.release_bike }.to raise_error('No bike available!')
+    end
   end
   it { is_expected.to respond_to(:dock).with(1).argument}
   it { is_expected.to respond_to(:bikes) }
@@ -14,9 +19,5 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bikes).to eq(bike)
-  end
-  it 'raises an error if no bike available' do
-    station = DockingStation.new
-    expect { station.release_bike }.to raise_error('No bike available!')
   end
 end
