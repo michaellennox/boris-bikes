974b91c5d1ea1877f969e5f3ae5987993c7d7436
emmabeynon <emma.beynon@gmail.com>
2015-11-24 13:51:37 +0000

    Refactored docking_station_spec test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,6 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it 'releases a working bike' do 
     bike = subject.release_bike
-    expect(bike.working?).to be_truthy
+    expect(bike).to be_working
   end
 end
