81feafa81fd03fbe8ab87acb67776a0ea995f0b1
giamir <giamir.buoncristiani@gmail.com>
2015-11-25 17:25:26 +0000

    adds protection to method working= of Bike class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,9 +1,13 @@
 class Bike
-  attr_accessor :working
+  attr_reader :working
   alias_method :working?, :working
+
+  def working=(arg)
+    fail 'have to be a bool' unless !!arg == arg
+    @working = arg
+  end
 
   def initialize
     @working = true
   end
-
 end
