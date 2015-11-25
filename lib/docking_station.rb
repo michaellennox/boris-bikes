927c8ff4301adae65150ba2ed62670e923870493
James Borrell <JamesBorrellsMac@Jamess-MacBook-Pro.local>
2015-11-25 18:31:23 +0000

    CH15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,23 @@ class DockingStation
   end
 
   def release_bike
-    fail "No more bikes available" if @bikes.empty?
+    fail "No more bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking Station is full" if @bikes.length == 20
+    fail "Docking Station is full" if full?
     @bikes << bike
+  end
+
+private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
