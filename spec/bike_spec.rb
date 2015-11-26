ab76b09b573d96b6fa21397eee61cb6713f774f4
michaellennox <michaelctlennox@gmail.com>
2015-11-26 15:19:21 +0000

    Added failing test to check Bike responds to report_broken method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,12 @@
 require 'bike'
 describe Bike do
-  it { is_expected.to respond_to :working?}  
+
+  describe '#working?' do
+    it { is_expected.to respond_to :working?}
+  end
+
+  describe '#report_broken' do
+    it { is_expected.to respond_to :report_broken }
+  end
+
 end
