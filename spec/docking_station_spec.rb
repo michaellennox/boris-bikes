001dd73831c2626d2223a58b51f2621689fce37e
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-26 10:51:39 +0000

    refactored docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,40 +1,43 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
   describe '#dock' do
+
     it 'raises an error if bike is already docked' do
       subject.dock(Bike.new)
-      expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
+      expect{subject.dock(Bike.new)}.to raise_error('Docking station full')
     end
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
   end
 
-  it { is_expected.to respond_to(:bike) }
+  describe '#bike' do
 
-  it 'should return the bike in the docking station' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    it { is_expected.to respond_to(:bike) }
+
   end
 
   describe '#release_bike' do
+
+    it { is_expected.to respond_to :release_bike }
+
     it  'raises an error if there are no bikes' do
-      expect{subject.release_bike}.to raise_error unless @bike
+      expect{subject.release_bike}.to raise_error('No bikes available')
     end
 
     it 'releases working bikes' do
-      bike = Bike.new
-      subject.dock(bike)
+      subject.dock(Bike.new)
       bike = subject.release_bike
       expect(bike).to be_working
     end
+
   end
 
 end
