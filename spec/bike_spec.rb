ee33226ee18aef8d0ed0f7784f12e1a540e17cd8
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-11-26 15:29:33 +0000

    passed test to ensure report_broken exists and created failing test for report_broken to change value of working? to false

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,6 +7,11 @@ describe Bike do
 
   describe '#report_broken' do
     it { is_expected.to respond_to :report_broken }
+
+    it 'when reported broken it assigns false to the working? variable' do
+      subject.report_broken
+      expect(subject.working?).to eq(false)
+    end
   end
 
 end
