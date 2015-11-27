8d43cacff16297726a4b1f51023e3c5bb9451dea
michaellennox <michaelctlennox@gmail.com>
2015-11-27 11:12:22 +0000

    Refactored DockingStation#release_bike tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,35 +1,38 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it 'to get bike out and working' do
-    bike = Bike.new
-    subject.dock(bike)
-    subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+
+    it 'should release a working bike' do
+      subject.dock(Bike.new)
+      expect(subject.release_bike).to be_working
+    end
+
+    it "should release the same bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
+
+    it "should raise an error when trying to release a broken bike" do
+      bike = Bike.new
+      bike.report_broken
+      subject.dock(bike)
+      expect{subject.release_bike}.to raise_error "The bike is broken"
+    end
+
+    it "should raise an error when there are no bikes available" do
+      expect{subject.release_bike}.to raise_error "No more bikes available"
+    end
+
   end
+
+  it { is_expected.to respond_to :release_bike }
 
   it {is_expected.to respond_to :bikes}
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it "returns same bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq(bike)
-  end
-
-  it "does not release broken bike" do
-    bike = Bike.new
-    bike.report_broken
-    subject.dock(bike)
-    expect{subject.release_bike}.to raise_error "The bike is broken"
-  end
-
-  it "no_bike_available" do
-    expect {subject.release_bike}.to raise_error "No more bikes available"
-  end
 
   it "Docking Station is full" do
     subject.capacity.times{subject.dock(Bike.new)}
