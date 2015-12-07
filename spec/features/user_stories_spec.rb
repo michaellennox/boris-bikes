6cc78cd49a54b1a1898b26f3a55ef954c5b7eae4
michaellennox <michaelctlennox@gmail.com>
2015-12-07 14:53:45 +0000

    Added two new user stories

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -20,4 +20,14 @@ describe 'User Stories' do
   end
 
 
+  # As a member of the public
+  # So I can return bikes I've hired
+  # I want to dock my bike at the docking station
+
+
+  # As a member of the public
+  # So I can decide whether to use the docking station
+  # I want to see a bike that has been docked
+
+
 end
