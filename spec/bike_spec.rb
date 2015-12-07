be3a7bdb5622beeac6c7578a961f5b1d75d1ae85
michaellennox <michaelctlennox@gmail.com>
2015-12-07 12:47:19 +0000

    Added failing test for working?

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,10 @@ describe Bike do
 
   subject(:bike) { described_class.new }
 
+  describe '#working?' do
+    it 'is expected to return true' do
+      expect(bike.working?).to be true
+    end
+  end
+
 end
