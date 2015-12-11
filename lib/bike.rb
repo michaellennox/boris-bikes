bf8ee4fb70594761cdf9a64e460d9608f5444c3b
michaellennox <michaelctlennox@gmail.com>
2015-12-11 15:58:07 +0000

    Passes feature test for reporting a broken bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,14 @@
 class Bike
 
-  def working?
-    true
+  attr_reader :working
+  alias_method :working?, :working
+
+  def initialize
+    @working = true
   end
-  
+
+  def report_broken
+    @working = false
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,8 +5,15 @@ describe Bike do
   subject(:bike) { described_class.new }
 
   describe '#working?' do
-    it 'is expected to return true' do
+    it 'is expected to return true upon initialization' do
       expect(bike.working?).to be true
+    end
+  end
+
+  describe '#report_broken' do
+    it 'is expected to change the status of working? to false' do
+      bike.report_broken
+      expect(bike.working?).to be false
     end
   end
 
